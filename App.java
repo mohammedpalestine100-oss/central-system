5bd86c35d674d3be3299ebe04b0df2fdb18ad240
mohammedpalestine100-oss <mohammed.palestine100@gmail.com>
2025-09-29 11:39:00 +0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.creata.poa;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CreataApp {
+public static void main(String[] args) {
+SpringApplication.run(CreataApp.class, args);
+}
+}
